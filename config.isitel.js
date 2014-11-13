b84a292c6110a04387db3fcd0110929e235f6b1d
speedmetal <stefano.gamma@gmail.com>
2014-11-13 10:30:49 +0100

    backup dir config

--- config.isitel.js
+++ config.isitel.js
@@ -10,11 +10,27 @@ config.mainRepository = {
 };
 
 config.repositories = [
-  {name: 'WAP.84', path: '\\\\10.30.254.84\\c$\\Javalab\\waportal\\WEB-INF\\lib\\'},
-  {name: 'WAP.88', path: '\\\\10.30.254.88\\c$\\Javalab\\waportal\\WEB-INF\\lib\\'},
-  {name: 'WAP.LISA', path: '\\\\lisa\\LISA-D\\Javalab\\wap-test\\WEB-INF\\lib\\'},
-  {name: 'PEM.88', path: '\\\\10.30.254.88\\c$\\Javalab\\wap-pem\\WEB-INF\\lib\\'},
-  {name: 'PEM.LISA', path: '\\\\lisa\\LISA-D\\Javalab\\wap-pem\\WEB-INF\\lib\\'}
+  {
+  	name: 'WAP.84',
+  	path: '\\\\10.30.254.84\\c$\\Javalab\\waportal\\WEB-INF\\lib\\',
+  	backup: '\\\\10.30.254.84\\c$\\Javalab\\waportal\\WEB-INF\\lib\\backup\\'
+  },{
+  	name: 'WAP.88',
+  	path: '\\\\10.30.254.88\\c$\\Javalab\\waportal\\WEB-INF\\lib\\',
+  	backup: '\\\\10.30.254.88\\c$\\Javalab\\waportal\\WEB-INF\\lib\\backup\\'
+  },{
+  	name: 'WAP.LISA',
+  	path: '\\\\lisa\\LISA-D\\Javalab\\wap-test\\WEB-INF\\lib\\',
+  	backup: '\\\\lisa\\LISA-D\\Javalab\\wap-test\\WEB-INF\\lib\\backup\\'
+  },{
+  	name: 'PEM.88',
+  	path: '\\\\10.30.254.88\\c$\\Javalab\\wap-pem\\WEB-INF\\lib\\',
+  	backup: '\\\\10.30.254.88\\c$\\Javalab\\wap-pem\\WEB-INF\\lib\\backup\\'
+  },{
+  	name: 'PEM.LISA',
+  	path: '\\\\lisa\\LISA-D\\Javalab\\wap-pem\\WEB-INF\\lib\\',
+  	backup: '\\\\lisa\\LISA-D\\Javalab\\wap-pem\\WEB-INF\\lib\\backup\\'
+  }
 ];
 
 module.exports = config;
